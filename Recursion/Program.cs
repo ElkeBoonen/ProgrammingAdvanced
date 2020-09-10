8a184108ad366afee5585e92064141e39a5daeaa
Elke Boonen <u0116591@thomasmore.be>
2020-09-10 22:02:15 +0200

    First, solve the problem. Then, write the code - John Johnson

---
+++ Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static int Factorial(int number)
+        {
+            int fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+
+        static int FactorialRecursive(int number)
+        {
+            if (number > 1) 
+                return (number * FactorialRecursive(number - 1));
+            else return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(10));
+            Console.WriteLine(FactorialRecursive(10));
+        }
+    }
+}
